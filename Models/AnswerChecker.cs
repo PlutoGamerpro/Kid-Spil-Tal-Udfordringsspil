633596a7dc3210b8afed0f7cd41a28e5ac228924
Gustav <114870315+PlutoGamerpro@users.noreply.github.com>
2024-10-17 14:42:29 +0200

    Add files via upload

---
+++ Models/AnswerChecker.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing; // For Color
+using System.Windows.Forms; // For TextBox
+
+namespace Børne_Spil
+{
+    public class AnswerChecker
+    {
+        public static bool AllAnswersCorrect { get; private set; }
+        public static int StreakScore;
+        public void CheckAnswers(int[] userInputs, Game game, TextBox[] textBoxes)
+        {
+            AllAnswersCorrect = true;
+
+            for (int i = 0; i < userInputs.Length; i++)
+            {
+                if (userInputs[i] == GetCorrectAnswer(i, game))
+                {
+                    textBoxes[i].BackColor = Color.LightGreen;
+                }
+                else
+                {
+                    textBoxes[i].BackColor = Color.Red;
+                    AllAnswersCorrect = false;
+                 
+                }
+            }
+        
+          
+
+
+        }
+
+        private int GetCorrectAnswer(int index, Game game)
+        {
+            switch (index)
+            {
+                case 0:
+                    return game.CorrectAnswerBox1;
+                case 1:
+                    return game.CorrectAnswerBox2;
+                case 2:
+                    return game.CorrectAnswerBox3;
+                case 3:
+                    return game.CorrectAnswerBox4;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(index));
+
+            }
+        }
+    }
+}
